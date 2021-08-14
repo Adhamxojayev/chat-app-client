a56a640af16cbe9963ab016c33fd194dea60c1e9
Ahror <axroradhamxojayev@gmail.com>
2021-08-14 16:17:30 +0500

    frontend host add

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,4 +1,4 @@
-let host = 'https://chat-backend-ap.herokuapp.com:4000'
+let host = 'https://chat-backend-ap.herokuapp.com'
 async function request (path,method,body,token){
     let headers = {
             'Content-Type' : 'application/json',
